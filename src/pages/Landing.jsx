231490875c448ffbaff1c295a6557188e319d001
Sticklo <mafimisebiadebayo@gmail.com>
2022-06-21 14:18:14 +0100

    mobile fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -11,12 +11,12 @@ const Landing = () => {
     <>
       <header>
         <nav className="flex item-center justify-between px-10 py-6">
-          <img src={Logo} alt="logo" className="w-[70px] h-[70px]" />
+          <img src={Logo} alt="logo" className=" w-[50px] h-[50px] md:w-[70px] md:h-[70px]" />
           <div>
             {/* <span className="font-medium text-3xl leading-[43px] mr-10">
               Sign in
             </span> */}
-            <span className="font-medium text-3xl leading-[43px] ">
+            <span className="font-medium text-2xl leading-[33px] md:text-3xl md:leading-[43px] ">
               Sign up
             </span>
           </div>
@@ -24,15 +24,15 @@ const Landing = () => {
       </header>
 
       <section className="w-full flex flex-col justify-center items-center">
-        <h1 className="font-bold text-[3.75rem] leading-[129.32px] text-center">
+        <h1 className="font-bold text-[3rem] leading-[90px] md:text-[3.75rem] md:leading-[129.32px] text-center">
           Bankless Card
         </h1>
-        <p className="font-normal text-3xl leading-[43.11px]  max-w-[59.5rem]">
+        <p className="font-normal text-2xl md:text-3xl leading-[33px] px-5 md:leading-[43.11px]  max-w-[59.5rem]">
           Bankless Card is an “affinity” payments platform designed to reward
           fiat spending with social and governance tokens from BanklessDAO,
           partner DAOs, and other Web3 ecosystems.
         </p>
-        <button className="p-2 bg-[#D02128] h-[3.563rem] w-[21.75rem] mt-4">
+        <button className="p-2 bg-[#D02128] h-[3rem] w-[15rem] md:h-[3.563rem] md:w-[21.75rem] mt-12">
           Sign up to get started
         </button>
       </section>
@@ -43,23 +43,23 @@ const Landing = () => {
       </section>
 
       <section className="flex flex-col justify-center items-center mt-[7rem]">
-        <h1 className="font-bold text-[64px] leading-[91.96px]">
+        <h1 className="font-bold text-[3rem] leading-[70px] text-center  md:text-[64px] md:leading-[91.96px]">
           Bankless Card Mobile App
         </h1>
         <img
           src={iphone}
           alt="iphone"
-          className="mt-[4rem] w-[33.402rem] h-[47.291rem]"
+          className="mt-[4rem] w-[20rem] h-[28rem] md:w-[30.402rem] md:h-[44.291rem]"
         />
-        <p className="font-normal text-[30px] leading-[43.11px]  max-w-[59.5rem] mt-[6rem]">
+        <p className="font-normal  text-2xl md:text-[30px] leading-[33px] px-5 md:leading-[43.11px]  max-w-[59.5rem] mt-[6rem]">
           Bankless Card is an “affinity” payments platform designed to reward
           fiat spending with social and governance tokens from BanklessDAO,
           partner DAOs, and other Web3 ecosystems.
         </p>
       </section>
 
-      <section className="mt-[10rem] flex flex-col md:flex-row justify-between px-[4rem]">
-        <div className="relative">
+      <section className="mt-[10rem] gap-[15rem] flex flex-col md:flex-row justify-between px-[2rem] md:px-[4rem]">
+        <div className="relative m-auto">
           <img
             src={bc_container}
             alt="bankless"
@@ -68,11 +68,11 @@ const Landing = () => {
           <img
             src={bc_container}
             alt="bankless"
-            className="rounded-[10px] absolute top-[8.5rem] left-[5.5rem] z-20"
+            className="rounded-[10px] absolute top-[6.5rem] left-[5.5rem] z-20 hidden md:block"
           />
         </div>
         <div>
-          <div className="flex justify-center items-center gap-8">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-8">
             <img src={bolt} alt="bolt" />
             <h1 className="font-bold text-[56px] leading-[67.86px] max-w-[599px]  ">
               Earn crypto back on every purchase
@@ -87,8 +87,8 @@ const Landing = () => {
       </section>
 
       <footer className="mt-[25rem] w-full">
-        <div className="flex flex-col justify-center items-center gap-6  bg-[#4F4F4F]  h-[192px] w-[85%] m-auto rounded-tr-[50px] rounded-tl-[50px]">
-          <span className="font-normal text-base leading-[25.74px] ">
+        <div className="flex flex-col justify-center items-center gap-6  bg-[#4F4F4F]  h-[192px] w-full md:w-[85%] m-auto rounded-tr-[50px] rounded-tl-[50px] px-4">
+          <span className="font-normal text-sm leading-[17px] md:text-base md:leading-[25.74px] ">
             Big ups to the BanklessDAO community for crafting the infrastructure
             and economy to make Bankless card possible.
           </span>
